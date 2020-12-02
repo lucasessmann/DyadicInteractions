29826df07c9f2d468109e90974f89dc4eff39844
Tom Hatton <tom-h@live.de>
2020-12-02 16:37:31 +0100

    Expanded EM functionality
    Expanded the EM functionality by
    - being able to select a rate at which a stimulus should be present
    - Dummy buttons for giving the response
    - Measuring RT between stimuli onset and pressing answering button
    
    All those variables can be accessed and eventually saved

--- Assets/Scripts/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager.cs
@@ -9,11 +9,16 @@ public class ExperimentManager : MonoBehaviour
     public GameObject spawnPointsGameObject;
     public GameObject distractorPrefab;
     public GameObject targetPrefab;
+    public float stimulusPresenceRate = 0.5f; 
     
     private List<Transform> _spawnPointsList = new List<Transform>();
     private bool _stimuliInScene = false;
     private GameObject[] _stimuliGOs;
     private List<Quaternion> _distractorDirectons = new List<Quaternion>();
+    private Random rnd = new Random();
+    private bool targetPresent;
+    private float stimuliOnsetTime;
+    private float lastReactionTime;
     private void Start()
     {
         _distractorDirectons.Add(Quaternion.Euler(-90,0,180));
@@ -36,6 +41,38 @@ public class ExperimentManager : MonoBehaviour
         {
             SpawnStimuli();
         }
+
+        // DUMMY ANSWERS 
+        //TODO: Connect with GUI buttons
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            lastReactionTime = Time.time - stimuliOnsetTime;
+            CheckAnswer(true);
+            //SpawnStimuli();
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            lastReactionTime = Time.time - stimuliOnsetTime;
+            CheckAnswer(false);
+            //SpawnStimuli();
+        }
+    }
+
+    private void CheckAnswer(bool response)
+    {
+        //DUMMY FEEBACK
+        // TODO: if response correct and target present -> Highlight Target!
+        if (targetPresent == response)
+        {
+            Debug.Log("Correct!");
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+        }
+        
+        Debug.Log("RT was " + lastReactionTime + " seconds");
     }
 
     private void SpawnStimuli()
@@ -48,27 +85,41 @@ public class ExperimentManager : MonoBehaviour
                 Destroy(stimulus);
             }
         }
+
+        targetPresent = rnd.NextDouble() < stimulusPresenceRate;
         
-        // Randomly select the target position among the spawn points and 
-        // create another list for distractor spawn points
-        var rnd = new Random();
-        var targetIndex = rnd.Next(_spawnPointsList.Count);
-        var distractorSpawnPoints = RemoveTargetIndexFromList(_spawnPointsList, targetIndex);
-        
-        // Spawn Target Prefab
-        Instantiate(targetPrefab, _spawnPointsList[targetIndex]);
-        
-        // Spawn Distractor Objects with randomly preselected orientation
-        foreach (var spawnPoint in distractorSpawnPoints)
+        if (targetPresent)
         {
-            var distractorDirection = _distractorDirectons[rnd.Next(_distractorDirectons.Count)];
-            var tmp_go = Instantiate(distractorPrefab, spawnPoint);
-            tmp_go.transform.GetChild(0).rotation = distractorDirection;
-        }
+            // Randomly select the target position among the spawn points and 
+            // create another list for distractor spawn points
         
+            var targetIndex = rnd.Next(_spawnPointsList.Count);
+            var distractorSpawnPoints = RemoveTargetIndexFromList(_spawnPointsList, targetIndex);
+        
+            // Spawn Target Prefab
+            Instantiate(targetPrefab, _spawnPointsList[targetIndex]);
+        
+            // Spawn Distractor Objects with randomly preselected orientation
+            foreach (var spawnPoint in distractorSpawnPoints)
+            {
+                var distractorDirection = _distractorDirectons[rnd.Next(_distractorDirectons.Count)];
+                var tmp_go = Instantiate(distractorPrefab, spawnPoint);
+                tmp_go.transform.GetChild(0).rotation = distractorDirection;
+            }
+        }
+        else
+        {
+            foreach (var spawnPoint in _spawnPointsList)
+            {
+                var distractorDirection = _distractorDirectons[rnd.Next(_distractorDirectons.Count)];
+                var tmp_go = Instantiate(distractorPrefab, spawnPoint);
+                tmp_go.transform.GetChild(0).rotation = distractorDirection;
+            }
+        }
         // Save GOs in list for later deletion.
         _stimuliGOs = GameObject.FindGameObjectsWithTag("stimulus");
         _stimuliInScene = true;
+        stimuliOnsetTime = Time.time;
     }
 
     // Method for removing TargetIndex from List of possible SpawnPoints. Return a new list with remaining spawn points
