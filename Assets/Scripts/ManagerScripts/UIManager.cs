44137c42cb89ce9407e9a775ed2c305d89e781bf
Lucas Essmann <lessmann@uos.de>
2020-12-09 15:29:16 +0100

    Menu with Data Capture and SubID

--- Assets/Scripts/ManagerScripts/UIManager.cs
+++ Assets/Scripts/ManagerScripts/UIManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject buttonCanvas;
     // Start is called before the first frame update
     void Start()
     {
+        // Finding the necessary canvasses
+        buttonCanvas = GameObject.Find("Buttons");
         
     }
 
