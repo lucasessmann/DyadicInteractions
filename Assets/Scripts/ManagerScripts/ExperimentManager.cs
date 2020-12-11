71effdf04d94bfaf705ab2784d87be0cbb2aa1b0
Tom <tom-h@live.de>
2020-12-11 17:10:02 +0100

    [CONTAINS SCENE MODS] Added Extended Spawning Functionality
    - Moved Spawning functionalities from experiment manager to spawning mangager
    - Basic Experiment Manager functionality in respective script
    - Visual Player Feedback for giving responses (highlighting target object)
    - IMplemented different stimuli sizes (21/35)

--- Assets/Scripts/ManagerScripts/ExperimentManager.cs
+++ Assets/Scripts/ManagerScripts/ExperimentManager.cs
@@ -2,17 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ExperimentManager : MonoBehaviour
 {
+
+    public float numberOfTrials;
+
+    private SpawningManager _spawningManager;
+    private SavingManager _savingManager;
+    private EyeTrackingManager _eyeTrackingManager;
+    private UIManager _uiManager;
+    private TimingManager _timingManager;
+    private float _lastReactionTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spawningManager = GetComponentInChildren<SpawningManager>();
+        _savingManager = GetComponentInChildren<SavingManager>();
+        _eyeTrackingManager = GetComponentInChildren<EyeTrackingManager>();
+        _uiManager = GetComponentInChildren<UIManager>();
+        _timingManager = GetComponentInChildren<TimingManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Just for testing
+        // TODO: delete later
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _spawningManager.SpawnStimuli();
+        }
+
+        // DUMMY ANSWERS 
+        //TODO: Connect with GUI buttons
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _lastReactionTime = Time.time - _spawningManager.stimuliOnsetTime;
+            CheckAnswer(true);
+            //SpawnStimuli();
+        }
         
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            _lastReactionTime = Time.time - _spawningManager.stimuliOnsetTime;
+            CheckAnswer(false);
+            //SpawnStimuli();
+        }
+
+    }
+    
+    private void CheckAnswer(bool response)
+    {
+        //DUMMY FEEBACK
+        // TODO: if response correct and target present -> Highlight Target!
+        if (_spawningManager.targetPresent == response)
+        {
+            Debug.Log("Correct!");
+            _spawningManager.GiveTargetFeedback();
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+            _spawningManager.GiveTargetFeedback();
+        }
+        
+        Debug.Log("RT was " + _lastReactionTime + " seconds");
     }
 }
--- Assets/Scripts/ManagerScripts/SpawningManager.cs
+++ Assets/Scripts/ManagerScripts/SpawningManager.cs
@@ -9,23 +9,37 @@ public class SpawningManager : MonoBehaviour
     public GameObject spawnPointsGameObject;
     public GameObject distractorPrefab;
     public GameObject targetPrefab;
-    public float stimulusPresenceRate = 0.5f; 
-    
+    private GameObject _targetGO;
+    public float stimulusPresenceRate = 0.5f;
+
+    private ExperimentManager _experimentManager;
+
     private List<Transform> _spawnPointsList = new List<Transform>();
-    private bool _stimuliInScene = false;
-    private GameObject[] _stimuliGOs;
+    private List<Transform> _chosenSpawnPoints;
     private List<Quaternion> _distractorDirectons = new List<Quaternion>();
-    private Random rnd = new Random();
-    private bool targetPresent;
-    private float stimuliOnsetTime;
-    private float lastReactionTime;
+
+    private bool _stimuliInScene = false;
+    public bool targetPresent;
+
+    private GameObject[] _stimuliGOs;
+    private Random _rnd = new Random();
+
+    private int[] _stimuliSizes = {21, 35};
+    private int _stimuliSize;
+    private int _targetIndex;
+
+    public float stimuliOnsetTime;
+    private float _lastReactionTime;
+
     private void Start()
     {
-        _distractorDirectons.Add(Quaternion.Euler(-90,0,180));
-        _distractorDirectons.Add(Quaternion.Euler(-90,0,0 ));
-        _distractorDirectons.Add(Quaternion.Euler(90,0,0 ));
-        _distractorDirectons.Add(Quaternion.Euler(90,0,180 ));
-       
+        _experimentManager = GetComponentInParent<ExperimentManager>();
+
+        _distractorDirectons.Add(Quaternion.Euler(-90, 0, 180));
+        _distractorDirectons.Add(Quaternion.Euler(-90, 0, 0));
+        _distractorDirectons.Add(Quaternion.Euler(90, 0, 0));
+        _distractorDirectons.Add(Quaternion.Euler(90, 0, 180));
+
         // Get transforms of every spawn point "cube" of the empty parent GO "SpawnPoints"
         foreach (Transform child in spawnPointsGameObject.transform)
         {
@@ -33,49 +47,7 @@ public class SpawningManager : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        // Just for testing
-        // TODO: delete later
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SpawnStimuli();
-        }
-
-        // DUMMY ANSWERS 
-        //TODO: Connect with GUI buttons
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            lastReactionTime = Time.time - stimuliOnsetTime;
-            CheckAnswer(true);
-            //SpawnStimuli();
-        }
-
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            lastReactionTime = Time.time - stimuliOnsetTime;
-            CheckAnswer(false);
-            //SpawnStimuli();
-        }
-    }
-
-    private void CheckAnswer(bool response)
-    {
-        //DUMMY FEEBACK
-        // TODO: if response correct and target present -> Highlight Target!
-        if (targetPresent == response)
-        {
-            Debug.Log("Correct!");
-        }
-        else
-        {
-            Debug.Log("Incorrect!");
-        }
-        
-        Debug.Log("RT was " + lastReactionTime + " seconds");
-    }
-
-    private void SpawnStimuli()
+    public void SpawnStimuli()
     {
         // If stimuli already in scene, delete before spawning new ones
         if (_stimuliInScene)
@@ -86,38 +58,44 @@ public class SpawningManager : MonoBehaviour
             }
         }
 
-        targetPresent = rnd.NextDouble() < stimulusPresenceRate;
-        
+        targetPresent = _rnd.NextDouble() < stimulusPresenceRate;
+
+        _stimuliSize = _stimuliSizes[_rnd.Next(_stimuliSizes.Length)];
+        _chosenSpawnPoints = _stimuliSize == 21 ? _spawnPointsList.GetRange(0, 21) : _spawnPointsList;
+
+
         if (targetPresent)
         {
             // Randomly select the target position among the spawn points and 
             // create another list for distractor spawn points
-        
-            var targetIndex = rnd.Next(_spawnPointsList.Count);
-            var distractorSpawnPoints = RemoveTargetIndexFromList(_spawnPointsList, targetIndex);
-        
+
+            _targetIndex = _rnd.Next(_chosenSpawnPoints.Count);
+            var distractorSpawnPoints = RemoveTargetIndexFromList(_chosenSpawnPoints, _targetIndex);
+
             // Spawn Target Prefab
-            Instantiate(targetPrefab, _spawnPointsList[targetIndex]);
-        
+            _targetGO = Instantiate(targetPrefab, _chosenSpawnPoints[_targetIndex]);
+
             // Spawn Distractor Objects with randomly preselected orientation
             foreach (var spawnPoint in distractorSpawnPoints)
             {
-                var distractorDirection = _distractorDirectons[rnd.Next(_distractorDirectons.Count)];
-                var tmp_go = Instantiate(distractorPrefab, spawnPoint);
-                tmp_go.transform.GetChild(0).rotation = distractorDirection;
+                var distractorDirection = _distractorDirectons[_rnd.Next(_distractorDirectons.Count)];
+                var tmpGameObject = Instantiate(distractorPrefab, spawnPoint);
+                tmpGameObject.transform.GetChild(0).rotation = distractorDirection;
             }
         }
         else
         {
-            foreach (var spawnPoint in _spawnPointsList)
+            foreach (var spawnPoint in _chosenSpawnPoints)
             {
-                var distractorDirection = _distractorDirectons[rnd.Next(_distractorDirectons.Count)];
-                var tmp_go = Instantiate(distractorPrefab, spawnPoint);
-                tmp_go.transform.GetChild(0).rotation = distractorDirection;
+                var distractorDirection = _distractorDirectons[_rnd.Next(_distractorDirectons.Count)];
+                var tmpGameObject = Instantiate(distractorPrefab, spawnPoint);
+                tmpGameObject.transform.GetChild(0).rotation = distractorDirection;
             }
         }
+
         // Save GOs in list for later deletion.
         _stimuliGOs = GameObject.FindGameObjectsWithTag("stimulus");
+        Debug.Log(_stimuliGOs);
         _stimuliInScene = true;
         stimuliOnsetTime = Time.time;
     }
@@ -128,9 +106,16 @@ public class SpawningManager : MonoBehaviour
         var listCount = list.Count;
         var result = new Transform[listCount - 1];
         list.CopyTo(0, result, 0, targetIndex);
-        list.CopyTo(targetIndex+1, result, targetIndex, listCount-1-targetIndex);
-        
+        list.CopyTo(targetIndex + 1, result, targetIndex, listCount - 1 - targetIndex);
+
         return new List<Transform>(result);
     }
-    
+
+    public void GiveTargetFeedback()
+    {
+        if (targetPresent)
+        {
+            _targetGO.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+        }
+    }
 }
