0e57c76e0b812cb3c151565080445249541c3b22
Lucas Essmann <lessmann@uos.de>
2020-12-14 12:41:15 +0100

    Testing script division

---
+++ Assets/Scripts/UI/OverlayMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class OverlayMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test Overlay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/PreMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PreMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Do sth");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
