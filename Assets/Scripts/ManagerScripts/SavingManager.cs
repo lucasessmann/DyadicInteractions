e12dd2134bc7b19279af6780b21ec2613d0c6351
DiGyt <dirk.guetlin@gmail.com>
2021-01-04 16:21:26 +0100

    updated saving manager
    updated manager to add suggestions from Farbod during our lecture, logging player sphere position and switching from csv to json serialization

--- Assets/Scripts/ManagerScripts/SavingManager.cs
+++ Assets/Scripts/ManagerScripts/SavingManager.cs
@@ -7,14 +7,17 @@ public class SavingManager : MonoBehaviour
 {
 	// path/file variables
 	public bool logData = true;
-	public float savingInterval = 1f;
+	public float loggingInterval = 0.01f;
+	public float time = 0f;
 	
 	
 	private string savePath;
-	private StreamWriter writer;
+	private DataLog dataLog;
 	
 	// variables to write
-	private int index = 0;
+	private int logIndex = 0;
+	private int trialNo = 1;  // TODO: get this value from the Experiment Manager instead
+	private int subjID = 0;  // TODO: get this value from the Experiment Manager instead
 	private System.DateTime sysTime;
 	
 	
@@ -22,15 +25,12 @@ public class SavingManager : MonoBehaviour
     void Start()
     {
 		sysTime = System.DateTime.Now;
-        savePath = Application.persistentDataPath + "/" + sysTime.ToString("yyyy-MM-dd_hh-mm-ss-ff") + ".csv";
+        savePath = Application.persistentDataPath + "/sub" + subjID.ToString() + "_trial" + trialNo.ToString() + ".json";
 		
 		// start saving the data
 		if (logData) {
-			writer = new StreamWriter(savePath, true);
-			writer.WriteLine("Index;SysTime;UnityTime;Player1Rot;Player2Rot;Finished");
-			writer.AutoFlush = true;
-		
-			StartCoroutine("SaveData");
+			dataLog = new DataLog();
+			StartCoroutine("appendLog");
 		}
 
     }
@@ -38,26 +38,60 @@ public class SavingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		time += Time.deltaTime;
+		// replace the following statement with if trial = start
+		if (time >= 30f && logData) {
+			time = 0f;
+			logIndex = 0;
+			trialNo ++;
+			
+			// save the datalog and create a new empty log
+			saveJson();
+			dataLog = new DataLog();
+			savePath = Application.persistentDataPath + "/sub" + subjID.ToString() + "_trial" + trialNo.ToString() + ".json";
 		
+		}
 		
 	}
 		
-	private IEnumerator SaveData() {
+	private IEnumerator appendLog() {
 		
-		// change this while loop to return False when the game loop is over
+		// TODO: change this while loop to return False when the game loop is over
 		while(1==1) {
 		//TODO: We use the current system datetime here
 		// It would be better to (additionally) derive this time
 		//from a game loop in the experimental manager
 		sysTime = System.DateTime.Now;
-        Debug.Log(sysTime.Second % 10);
 		
-		writer.WriteLine(index + ";" + sysTime + ";" + Time.time + ";0;0;0");
-		Debug.Log("Got until iteration: " + index);
-		index ++;
-		yield return new WaitForSeconds(savingInterval);
+		// add all the variables that we want to log
+		dataLog.index.Add(logIndex);
+		dataLog.sysTime.Add(sysTime.ToString("yyyy-MM-dd_hh-mm-ss-ff"));
+		dataLog.runTime.Add(Time.time);
+		dataLog.remoteGazePos.Add(this.transform.parent.GetComponent<ExperimentManager>().RemoteGazeSphere.position);
+		dataLog.localGazePos.Add(this.transform.parent.GetComponent<ExperimentManager>().LocalGazeSphere.position);
+		
+		logIndex ++;
+		yield return new WaitForSeconds(loggingInterval);
 		}
+	}
+	
+	private void saveJson() {
+		string stringDataLog = JsonUtility.ToJson(dataLog);
+		System.IO.File.WriteAllText(savePath, stringDataLog);
 	}
 	
 	
 }
+
+
+[System.Serializable]
+public class DataLog {
+	// introduce all variables that we want to log
+    public int trialNo;
+    public List<int> index = new List<int>();
+	public List<string> sysTime = new List<string>();
+	public List<float> runTime = new List<float>();
+	public List<Vector3> remoteGazePos = new List<Vector3>();
+	public List<Vector3> localGazePos = new List<Vector3>();
+	
+	}
